b3ff6a54078a34c0ca106c0ff20a88a3dc8e872d
irwansah <irwansahrulsidik@gmail.com>
2023-08-24 20:32:00 +0700

    update a

--- lib/test.dart
+++ lib/test.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter irwansah irwan',
+      title: 'Flutter irwansah irwans',
       theme: ThemeData(
         // This is the theme of your application.
         //
